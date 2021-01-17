325964085dde1952f363c32bf2917fe5583c31b0
huhinka <huhinka@163.com>
2021-01-17 23:18:19 +0800

    添加 express routes

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 import express from 'express'
+import path from 'path'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
 
+import indexRouter from './routes/index.js'
+import usersRouter from './routes/users.js'
+
+const { json, urlencoded, static: expressStatic } = express
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
+app.use(logger('dev'))
+app.use(json())
+app.use(urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(expressStatic(path.join(path.resolve(), 'public')))
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+export default app
