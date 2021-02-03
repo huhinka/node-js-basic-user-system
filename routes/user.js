87c8033dea2e40815df80fcdcbaf4d366a3af10b
huhinka <huhinka@163.com>
2021-02-03 11:33:08 +0800

    refact routes by async

--- routes/user.js
+++ routes/user.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { BadRequestError, GeneralError } from '../error.js'
 
 import jwt from './jwt.js'
 import UserModel from './user.model.js'
@@ -10,51 +11,40 @@ const router = Router()
  *
  * Protected by JWT Auth.
  */
-router.get('/', jwt, (req, res) => {
+router.get('/', jwt, async (req, res) => {
   if (!req.user.admin) {
     return res.sendStatus(401)
   } else {
-    const query = UserModel.find({})
-
-    query.exec((err, users) => {
-      if (err) {
-        res.sendStatus(400).send(err)
-      } else {
-        res.json(users)
-      }
-    })
+    const users = await UserModel.find({}).exec()
+    res.json(users)
   }
 })
 
 /**
  * POST a user
  */
-router.post('/', (req, res) => {
+router.post('/', async (req, res, next) => {
   const newUser = new UserModel({
     name: req.body.name,
     password: req.body.password
   })
 
-  UserModel.find({ name: req.body.name }).exec((err, users) => {
-    if (err) {
-      res.send(400).send(err)
-    } else {
-      if (users.length !== 0) {
-        res.send(400).send(`User ${req.body.name} exists.`)
+  const users = await UserModel.find({ name: req.body.name }).exec()
+
+  if (users.length !== 0) {
+    next(new BadRequestError(`User ${req.body.name} exists.`))
+  } else {
+    newUser.save((err, user) => {
+      if (err) {
+        next(new GeneralError(err))
       } else {
-        newUser.save((err, user) => {
-          if (err) {
-            res.send(400).send(err)
-          } else {
-            res.json({
-              msg: 'User successfully added!',
-              user
-            })
-          }
+        res.json({
+          msg: 'User successfully added!',
+          user
         })
       }
-    }
-  })
+    })
+  }
 })
 
 export default router
